cc012c484d81e460964f7720bacce4cb8c2d4bb4
lucasMuner <146034160+lucasMuner@users.noreply.github.com>
2023-11-11 13:38:59 -0300

    Add files via upload

--- data.js
+++ data.js
@@ -1,14 +1,35 @@
-
-
 const temperaturaData = [];
-const ctx = document.getElementById('myChart');
-const chart = new Chart(ctx, {
+const ctx = document.getElementById('chartTemp');
+const chartTemp = new Chart(ctx, {
   type: 'line',
   data: {
     labels: ["1","2","3","4","5","6","7","8","9","10"], // Inicialmente, nenhum rótulo
     datasets: [{
       label: 'Temperatura',
       data: temperaturaData, // Usará os dados de temperatura
+      borderWidth: 1,
+      borderColor: 'rgba(255, 99, 132, 1)', // Cor da linha (R, G, B, A)
+      pointBackgroundColor: 'rgba(255, 99, 132, 1)', // Cor do ponto (R, G, B, A)
+
+    }]
+  },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true
+      }
+    }
+  }
+});
+const umidadeData = [];
+const cty = document.getElementById('chartUmidade');
+const chartUmidade = new Chart(cty, {
+  type: 'line',
+  data: {
+    labels: ["1","2","3","4","5","6","7","8","9","10"], // Inicialmente, nenhum rótulo
+    datasets: [{
+      label: 'Umidade',
+      data: umidadeData, // Usará os dados de temperatura
       borderWidth: 1,
 
     }]
@@ -22,16 +43,39 @@ const chart = new Chart(ctx, {
   }
 });
 
-function updateTemperatureChart(newTemperature) {
+const lumiData = [];
+const ctz = document.getElementById('chartLuminosidade');
+const chartLumi = new Chart(ctz, {
+  type: 'line',
+  data: {
+    labels: ["1","2","3","4","5","6","7","8","9","10"], // Inicialmente, nenhum rótulo
+    datasets: [{
+      label: 'Luminosidade',
+      data: lumiData, // Usará os dados de temperatura
+      borderWidth: 1,
+      borderColor: 'rgba(255, 255, 0, 1)', // Cor da linha para luminosidade (R, G, B, A)
+      pointBackgroundColor: 'rgba(255, 255, 0, 1)', // Cor do ponto para luminosidade (R, G, B, A)
+    }]
+  },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true
+      }
+    }
+  }
+});
+
+function updateChart(newData, graph, dadosArr) {
   // Adicione o novo valor ao array de dados de temperatura
-  temperaturaData.push(newTemperature);
+  dadosArr.push(newData);
 
   // Limita o número de pontos no gráfico para, por exemplo, mostrar apenas os últimos 10 valores
-  if (temperaturaData.length > 10) {
-    temperaturaData.shift();
+  if (dadosArr.length > 10) {
+    dadosArr.shift();
   }
   // Atualize o gráfico
-  chart.update();
+  graph.update();
 }
 
 function fetchDataDataBase(){
@@ -44,8 +88,10 @@ function fetchDataDataBase(){
     document.querySelector("#luminosidade").textContent = `Luminosidade: ${data.luminosidade} Lux`;
     document.querySelector("#setPointTemp").textContent = `SP Temperatura: ${data.setPointTemperatura} °C`;
 
-    updateTemperatureChart(data.temperatura);
-    
+    updateChart(data.temperatura, chartTemp, temperaturaData);
+    updateChart(data.umidade, chartUmidade, umidadeData);
+    updateChart(data.luminosidade, chartLumi, lumiData);
+   
     const form = document.querySelector("form");
 
   form.addEventListener('submit', (event) => {
@@ -54,8 +100,17 @@ function fetchDataDataBase(){
     // Obtenha os novos valores dos campos de input
     const setPointTempInput = document.getElementById("setPointTempInput");
 
+    const resetCheckbox = document.getElementById('cb3-8');
+    const lampCheckbox = document.getElementById('cb3-7');
+
+
+    const isResetChecked = resetCheckbox.checked;
+    const isLampChecked = lampCheckbox.checked;
+
     const newSetPoints = {
       setPointTemperatura: setPointTempInput.value,
+      lampadaLigada: isLampChecked,
+      resetarEsp: isResetChecked
     };
     
     // Faça a solicitação fetch para atualizar o banco de dados com os novos valores
@@ -97,3 +152,29 @@ function fetchDataDataBase(){
 fetchDataDataBase();
 
 setInterval(fetchDataDataBase, 10000);
+
+/*const resetCheckbox = document.getElementById('cb3-8');
+
+// Obter o valor da checkbox
+const isResetChecked = resetCheckbox.checked;
+
+// Exemplo de uso
+if (isResetChecked) {
+  console.log('A checkbox "Resetar Sistema" está marcada.');
+} else {
+  console.log('A checkbox "Resetar Sistema" não está marcada.');
+}
+
+const lampCheckbox = document.getElementById('cb3-7');
+
+// Obter o valor da checkbox
+const isLampChecked = lampCheckbox.checked;
+
+// Exemplo de uso
+if (isLampChecked) {
+  console.log('A checkbox "Lampada" está acesa.');
+} else {
+  console.log('A checkbox "Lampada" não está acesa.');
+}
+*/
+
